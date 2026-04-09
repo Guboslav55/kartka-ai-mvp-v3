80c10049ff269d9c792396a4dbc2e2eec5a7cd06
Guboslav55 <guboslav55@gmail.com>
2026-04-09 23:19:07 +0300

    Add files via upload

--- kartka-ai/next.config.js
+++ kartka-ai/next.config.js
@@ -4,11 +4,15 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'oaidalleapiprodscus.blob.core.windows.net',
-      },
+      { protocol: 'https', hostname: 'oaidalleapiprodscus.blob.core.windows.net' },
+      { protocol: 'https', hostname: '*.supabase.co' },
+      { protocol: 'https', hostname: 'replicate.delivery' },
     ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '20mb',
+    },
   },
 };
 
